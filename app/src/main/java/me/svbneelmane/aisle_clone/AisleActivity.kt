4b3fd9279853f1598a9180dd02df39ace068dbd2
shivaprasad-bhat <shivaprasad@svbneelmane.in>
2022-09-24 15:49:17 +0530

    feat(): Adding initial project code with gradle config

---
+++ app/src/main/java/me/svbneelmane/aisle_clone/AisleActivity.kt
@@ -0,0 +1,11 @@
+package me.svbneelmane.aisle_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AisleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aisle)
+    }
+}
